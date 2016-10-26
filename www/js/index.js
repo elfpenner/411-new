2cd5975574ded3ce5159df99672948cd6e803bd8
elfpenner <ddeveling@gmail.com>
2016-10-26 23:37:11 +0200

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -45,10 +45,28 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;'); */
 
         //console.log('Received Event: ' + id);
-        
+        checkConnection();
         //var targetUrl = "http://www.following-and-fishing.eu/app/";
         //window.location.replace = targetUrl;
         window.location = "http://www.following-and-fishing.eu/app/";
         
     }
 };
+
+function checkConnection() {
+    var networkState = navigator.connection.type;
+ 
+    var states = {};
+    states[Connection.UNKNOWN]  = 'Unknown connection';
+    states[Connection.ETHERNET] = 'Ethernet connection';
+    states[Connection.WIFI]     = 'WiFi connection';
+    states[Connection.CELL_2G]  = 'Cell 2G connection';
+    states[Connection.CELL_3G]  = 'Cell 3G connection';
+    states[Connection.CELL_4G]  = 'Cell 4G connection';
+    states[Connection.CELL]     = 'Cell generic connection';
+    states[Connection.NONE]     = 'No network connection';
+ 
+    alert('Connection type: ' + states[networkState]);
+}
+ 
+checkConnection();
